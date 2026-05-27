7d063c282e5983e5d6bc60f83c0b0c7225a734d5
Gyuri <ygreee0320@naver.com>
2026-05-27 16:39:21 +0900

    #19 feat: Swagger UI JWT 인증 설정 추가

---
+++ service-core/src/main/java/com/kkpp/core/global/config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package com.kkpp.core.global.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "KKPP API",
+                version = "v1",
+                description = "농업인 한도 기반 외상 구매 서비스 API"
+        ),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        description = "로그인 후 발급된 Access Token을 입력하세요. (Bearer 접두사 불필요)"
+)
+public class OpenApiConfig {
+}
