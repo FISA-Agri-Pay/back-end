156b19811ec95c191872ab6e43a4e6636483d7ba
cuterrabbit <ledw8992@naver.com>
2026-05-31 01:21:21 +0900

    #51 feat: 관리자 BNPL Swagger 테스트 설정 추가

---
+++ service-admin/src/main/java/com/kkpp/admin/global/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.kkpp.admin.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    private static final String BEARER_SCHEME = "bearerAuth";
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("KKPP Admin API")
+                        .version("v1")
+                        .description("농협은행 BNPL 관리자 서비스 API 문서"))
+                .addSecurityItem(new SecurityRequirement().addList(BEARER_SCHEME))
+                .components(new Components()
+                        .addSecuritySchemes(BEARER_SCHEME, new SecurityScheme()
+                                .name(BEARER_SCHEME)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .description("로그인 후 발급된 Access Token을 입력하세요")));
+    }
+}
