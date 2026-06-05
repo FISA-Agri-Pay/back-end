17f309262e6e693bb3b4d59bc20c322b189e11dc
Gyuri <ygreee0320@naver.com>
2026-06-06 01:31:33 +0900

    #74 feat: 관리자 서류 조회 시 S3 Presigned URL 발급으로 전환

--- service-admin/src/main/java/com/kkpp/admin/global/config/S3Config.java
+++ service-admin/src/main/java/com/kkpp/admin/global/config/S3Config.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Profile;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
 @Profile("prod")
@@ -15,6 +16,14 @@ public class S3Config {
     @Bean
     public S3Client s3Client(@Value("${cloud.aws.s3.region:ap-northeast-2}") String region) {
         return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+    }
+
+    @Bean
+    public S3Presigner s3Presigner(@Value("${cloud.aws.s3.region:ap-northeast-2}") String region) {
+        return S3Presigner.builder()
                 .region(Region.of(region))
                 .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
