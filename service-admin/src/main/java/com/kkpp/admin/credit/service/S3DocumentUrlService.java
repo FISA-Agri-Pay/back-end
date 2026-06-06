cafdde60af831fea5d93c817fe8adb7b1133d2a1
Gyuri <ygreee0320@naver.com>
2026-06-06 14:58:08 +0900

    #74 feat: service-admin dev 프로파일에서 S3 연동 활성화

--- service-admin/src/main/java/com/kkpp/admin/credit/service/S3DocumentUrlService.java
+++ service-admin/src/main/java/com/kkpp/admin/credit/service/S3DocumentUrlService.java
@@ -8,7 +8,7 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
 
 @Service
-@Profile("prod")
+@Profile("prod | dev")
 public class S3DocumentUrlService implements DocumentUrlService {
 
     private final S3Presigner s3Presigner;
--- service-admin/src/main/java/com/kkpp/admin/global/config/S3Config.java
+++ service-admin/src/main/java/com/kkpp/admin/global/config/S3Config.java
@@ -10,7 +10,7 @@ import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
-@Profile("prod")
+@Profile("prod | dev")
 public class S3Config {
 
     @Bean
