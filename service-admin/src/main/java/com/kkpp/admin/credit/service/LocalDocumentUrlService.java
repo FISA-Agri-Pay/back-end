78a38fbe531979cb9c5a24ce1ce1b6f37429cef1
Gyuri <ygreee0320@naver.com>
2026-06-06 15:23:27 +0900

    #74 fix: admin dev 프로파일 S3 연동 적용

--- service-admin/src/main/java/com/kkpp/admin/credit/service/LocalDocumentUrlService.java
+++ service-admin/src/main/java/com/kkpp/admin/credit/service/LocalDocumentUrlService.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile("!prod")
+@Profile("local")
 public class LocalDocumentUrlService implements DocumentUrlService {
 
     @Override
--- service-admin/src/main/java/com/kkpp/admin/credit/service/S3DocumentUrlService.java
+++ service-admin/src/main/java/com/kkpp/admin/credit/service/S3DocumentUrlService.java
@@ -8,7 +8,7 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
 
 @Service
-@Profile("prod | dev")
+@Profile("prod | dev | s3")
 public class S3DocumentUrlService implements DocumentUrlService {
 
     private final S3Presigner s3Presigner;
