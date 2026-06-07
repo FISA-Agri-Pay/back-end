319749b783cc50d2563190f5225f839e029cba3d
SA JAEHEON <113874212+Zaixian5@users.noreply.github.com>
2026-06-07 22:57:40 +0900

    Revert "#60 deploy: service-catalog ECR/EKS 실시간 배포 구현"

--- service-catalog/src/main/java/com/kkpp/catalog/global/config/SecurityConfig.java
+++ service-catalog/src/main/java/com/kkpp/catalog/global/config/SecurityConfig.java
@@ -2,13 +2,13 @@ package com.kkpp.catalog.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-// import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-// @Profile("local")
+@Profile("local")
 public class SecurityConfig {
 
     @Bean
