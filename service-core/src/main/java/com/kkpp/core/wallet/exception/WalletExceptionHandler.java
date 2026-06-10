83bf4d8480de65ce395aa52ddb71b4bdea0a6aea
Gyuri <ygreee0320@naver.com>
2026-06-10 09:56:56 +0900

    #112 fix: WalletExceptionHandler Order 설정으로 지갑 미존재 시 404 응답

--- service-core/src/main/java/com/kkpp/core/wallet/exception/WalletExceptionHandler.java
+++ service-core/src/main/java/com/kkpp/core/wallet/exception/WalletExceptionHandler.java
@@ -3,11 +3,13 @@ package com.kkpp.core.wallet.exception;
 import com.kkpp.common.core.response.ApiResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
+@Order(1)
 @RestControllerAdvice
 public class WalletExceptionHandler {
 
